84c3976f72bef2ba35821c9cec514dc207020b3b
@DEAD-BILLAH-SHOBUZ <arifbillahmahamudaamena@gmail.com>
2023-12-05 09:00:55 +0600

    problem

--- app/Http/Controllers/backend/AdminController.php
+++ app/Http/Controllers/backend/AdminController.php
@@ -32,6 +32,48 @@ class AdminController extends Controller
     $admins = Admin::find($id);
     return view('backend.pages.admin_profile.edit', compact('admins'));
   }
+  public function update(Request $request)
+  {
+
+    dd($request->all());
+    $validate = Validator::make($request->all(), [
+      'first_name' => 'required',
+      'last_name' => 'required',
+      'gmail' => 'required',
+      'phone' => 'required',
+      'birth_day' => 'required',
+      'address' => 'required',
+      'gender' => 'required',
+      'password' => 'required',
+    ]);
+    if ($validate->fails()) {
+      return redirect()->back()->withErrors($validate);
+    }
+    // dd('hello');
+    $fileName = null;
+    if ($request->hasFile('image')) {
+      $file = $request->file('image');
+      $fileName = date('Ymdhis') . '.' . $file->getClientOriginalExtension();
+      // $destination = "uploads";
+      // $file->move($destination, $fileName);
+
+      $file->move("uploads", $fileName);
+    }
+    // dd($request->all());
+    Admin::create([
+      'first_name' => $request->first_name,
+      'last_name' => $request->last_name,
+      'gmail' => $request->gmail,
+      'phone' => $request->phone,
+      'password' => $request->password,
+      'birth_day' => $request->birth_day,
+      'address' => $request->address,
+      'role' => $request->role,
+      'image' => $fileName,
+      'gender' => $request->gender
+    ]);
+    return redirect()->route('admin.list');
+  }
 
 
 
@@ -75,50 +117,9 @@ class AdminController extends Controller
   {
     return view("backend.pages.admin.form");
   }
-  public function store(Request $request)
-  {
+  
 
-    // dd($request->all());
-    $validate = Validator::make($request->all(), [
-      'first_name' => 'required',
-      'last_name' => 'required',
-      'gmail' => 'required',
-      'phone' => 'required',
-      'birth_day' => 'required',
-      'address' => 'required',
-      'gender' => 'required',
-      'password' => 'required',
-    ]);
-    if ($validate->fails()) {
-      return redirect()->back()->withErrors($validate);
-    }
-    // dd('hello');
-    $fileName = null;
-    if ($request->hasFile('image')) {
-      $file = $request->file('image');
-      $fileName = date('Ymdhis') . '.' . $file->getClientOriginalExtension();
-      // $destination = "uploads";
-      // $file->move($destination, $fileName);
-
-      $file->move("uploads", $fileName);
-    }
-    // dd($request->all());
-    Admin::create([
-      'first_name' => $request->first_name,
-      'last_name' => $request->last_name,
-      'gmail' => $request->gmail,
-      'phone' => $request->phone,
-      'password' => $request->password,
-      'birth_day' => $request->birth_day,
-      'address' => $request->address,
-      'role' => $request->role,
-      'image' => $fileName,
-      'gender' => $request->gender
-    ]);
-    return redirect()->route('admin.list');
-  }
-
-  public function update(Request $request, $id)
+  public function store(Request $request, $id)
   {
     $admins = Admin::find($id);
     //        dd($admins);
--- resources/views/backend/pages/admin_profile/edit.blade.php
+++ resources/views/backend/pages/admin_profile/edit.blade.php
@@ -1,82 +1,82 @@
 @extends("backend.master")
 @section('content')
-    <main class="fluid ">
-        <div class="container col-6">
-            <h1>
-                Input your Information!
-            </h1>
-            <form action="{{route('update.admin',$admins->id)}}" method="post" class="was-validated" enctype="multipart/form-data">
-                @method('put');
-                @csrf
-                <div class="form-group">
-                    <label for="exampleInputEmail1">First Name</label>
-                    <input type="text" value="Null" name="first_name" class="form-control is-valid" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your First Name" required>
-                </div>
-                @error('first_name')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Last Name</label>
-                    <input type="text" value="Null" name="last_name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your Last Name" required>
-                </div>
-                @error('last_name')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email</label>
-                    <input type="email" value="{{$admins->gmail}}" name="gmail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your email" required>
-                </div>
-                @error('email')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Phone</label>
-                    <input type="number" value="{{$admins->phone}}" name="phone" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your Unique Phone Number" required>
-                </div>
-                @error('phone')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
+<main class="fluid ">
+    <div class="container col-6">
+        <h1>
+            Edit your Information!
+        </h1>
+        <form action="{{route('update.admin',$admins->id)}}" method="post" class="was-validated" enctype="multipart/form-data">
+           
+            @csrf
+            <div class="form-group">
+                <label for="exampleInputEmail1">First Name</label>
+                <input type="text" name="first_name" class="form-control is-valid" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your First Name" required>
+            </div>
+            @error('first_name')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+            <div class="form-group">
+                <label for="exampleInputEmail1">Last Name</label>
+                <input type="text" name="last_name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your Last Name" required>
+            </div>
+            @error('last_name')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email</label>
+                <input type="email" value="{{$admins->gmail}}" name="gmail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your email" required>
+            </div>
+            @error('email')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+            <div class="form-group">
+                <label for="exampleInputEmail1">Phone</label>
+                <input type="number" value="{{$admins->phone}}" name="phone" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your Unique Phone Number" required>
+            </div>
+            @error('phone')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
 
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Role</label>
-                    <select name="role" value="{{$admins->role}}" id="" class="form-control" required>
-                        <option value="admin">Admin</option>
-                        <option value="manage">manegar</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Date of Birth</label>
-                    <input type="date" value="{{$admins->birth_day}}" name="birth_day" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your email" required>
-                </div>
-                @error('birth_day')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Address</label>
-                    <input type="text" value="{{$admins->address}}" name="address" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your email" required>
-                </div>
-                @error('address')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Image</label>
-                    <input type="file"  name="image" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your email">
-                </div>
-                @error('image')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Gender</label>
-                    <select name="gender" value="{{$admins->gender}}" id="" class="form-control" required>
-                        <option value="mail">mail</option>
-                        <option value="femail">femail</option>
-                    </select>
-                </div>
-                @error('gender')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                <button type="submit" class="btn btn-primary mt-2">Submit</button>
-            </form>
-        </div>
-    </main>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Role</label>
+                <select name="role" value="{{$admins->role}}" id="" class="form-control" required>
+                    <option value="admin">Admin</option>
+                    <option value="manage">manegar</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Date of Birth</label>
+                <input type="date" name="birth_day" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+            </div>
+            @error('birth_day')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+            <div class="form-group">
+                <label for="exampleInputEmail1">Address</label>
+                <input type="text" name="address" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your address" required>
+            </div>
+            @error('address')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+            <div class="form-group">
+                <label for="exampleInputEmail1">Image</label>
+                <input type="file" name="image" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            @error('image')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+            <div class="form-group">
+                <label for="exampleInputEmail1">Gender</label>
+                <select name="gender" id="" class="form-control" required>
+                    <option value="mail">mail</option>
+                    <option value="femail">femail</option>
+                </select>
+            </div>
+            @error('gender')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+            <button type="submit" class="btn btn-primary mt-2">Submit</button>
+        </form>
+    </div>
+</main>
 @endsection
